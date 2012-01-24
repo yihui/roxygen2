395ba0610783f5f59bcb83902d8805a21f185f1a
Manuel J. A. Eugster <manuel@mjae.net>
2012-01-24 08:52:36 -0700

    example pkg

---
+++ sandbox/mjaepkg/R/a.R
@@ -0,0 +1,17 @@
+
+#' A very complex function.
+#' @param a A number
+#' @return The number plus one
+#' @export
+mjaefun <- function(a) {
+  1 + a
+}
+
+
+
+#' Now comes a missssssspelllllllleeeeedddd word.
+#' @param x A number
+#' @return The sq%&Euare root of x
+blub <- function(x) {
+  sqrt(x)
+}
---
+++ sandbox/mjaepkg/demo/aaa.R
@@ -0,0 +1,11 @@
+#' @demo This is a test demo.
+#'
+#' @details
+#'   And this is the multiline
+#'   description.
+#'
+
+library("ggplot2")
+
+a <- 1 + 1
+
